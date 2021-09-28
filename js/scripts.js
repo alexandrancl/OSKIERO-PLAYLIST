b6425efff4c69f36357d569eb0ed22f593938303
Alexandra Duran <duranalexandra2004@gmail.com>
2021-09-28 00:18:34 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,75 @@
+const list_container = document.getElementById("list-container");
+const audio = document.getElementById("audio")
+const favs = [
+    {
+        id: 1,
+        title: "A tu merced",
+        audio: "audio/A tu merced",
+        cover: "img/YHLQMDLG.JPG",
+        artist: "Bad Bunny"
+    },
+    {
+        id: 2,
+        title: "Ahora me llama",
+        audio: "audio/Ahora me llama",
+        cover: "img/unstoppable.JPG",
+        artist: "Karol G"
+    },
+    {
+        id: 3,
+        title: "Ajena",
+        audio: "audio/Ajena",
+        cover: "img/ajena.JPG",
+        artist: "Myke Towers"
+    },
+    {
+        id: 4,
+        title: "Amorfoda",
+        audio: "audio/Amorfoda",
+        cover: "img/amorfoda.JPG",
+        artist: "Bad Bunny"
+    },
+    {
+        id: 5,
+        title: "Asesina",
+        audio: "audio/Asesina",
+        cover: "img/asesina.JPG",
+        artist: "Brytiago"
+    },
+    
+    {
+        id: 6,
+        title: "Candy",
+        audio: "audio/Candy",
+        cover: "img/love_and_sex.JPG",
+        artist: "Plan B"
+    },
+    {
+        id: 7,
+        title: "Dákiti",
+        audio: "audio/Dákiti",
+        cover: "img/el_último_tour.JPG",
+        artist: "Bad Bunny"
+    },
+    {
+        id: 8,
+        title: "Downtown",
+        audio: "audio/Downtown",
+        cover: "img/downtown.JPG",
+        artist: "J Balvin"
+    },
+    {
+        id: 9,
+        title: "Dream Girl (Remix)",
+        audio: "audio/Dream Girl",
+        cover: "img/dream girl.JPG",
+        artist: "ir Sairs"
+    },
+    {
+        id: 10,
+        title: "Es un secreto",
+        audio: "audio/Es un secreto",
+        cover: "img/house_of_pleasure.JPG",
+        artist: "Plan B"
+    },
+]
